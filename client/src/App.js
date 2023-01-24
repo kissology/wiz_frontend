bcc4abd8c97bc0aa8eb2b26ab27fd2a088fd4ba5
Yetunde Alese <yaalese1@gmail.com>
2023-01-24 12:14:36 -0500

    map render to dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
-import { useState, useEffect } from "react";
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieWFhbGVzZTEiLCJhIjoiY2xkYWZpMDJhMDNjZjNwdDRkYzA3YXN3ciJ9.KS4BtA3Gn4wTKb_KMGqPbQ';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  const mapContainer = useRef(null);
+const map = useRef(null);
+const [lng, setLng] = useState(-74.0060);
+const [lat, setLat] = useState( 40.7128);
+const [zoom, setZoom] = useState(9);
+ 
+useEffect(() => {
+if (map.current) return; // initialize map only once
+map.current = new mapboxgl.Map({
+container: mapContainer.current,
+style: 'mapbox://styles/mapbox/streets-v12',
+center: [lng, lat],
+zoom: zoom
+});
+});
+ 
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+     <div className="sidebar">
+Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+</div>
+<div ref={mapContainer} className="map-container" />
     </div>
   );
 }
