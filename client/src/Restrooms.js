66deebe78f7fa965ad59e8a9fc7645e59a4ed0db
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2023-01-26 21:24:04 -0500

    Render Ratings

--- client/src/Restrooms.js
+++ client/src/Restrooms.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Restrooms(){
+const [ratings, setRatings] = useState([])
+
+useEffect(() => {
+    fetch("http://localhost:3000/ratings")
+    .then(res => res.json())
+    .then(console.log('asshole'))
+},[]);
+
+
+
+
+
 
     return (
     <div>Restrooms Page!</div>
